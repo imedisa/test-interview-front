5e413ee9f7ef1ff9593c3a2c0acb23c404d1b83c
Medisa <medisayep@gmail.com>
2024-07-26 19:17:48 +0330

    header fixed

--- zip-p/main.js
+++
@@ -1,8 +0,0 @@
-import "./assets/style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
-import Alpine from "alpinejs";
-window.Alpine = Alpine;
-Alpine.start();
-
